463f640c4d2f95c8fe37f75089ea7a686b183d5e
Raghav Sharma <raghav_tinker@yahoo.com>
2021-10-08 20:18:12 +0530

    issue with post req

--- api/app.py
+++ api/app.py
@@ -73,7 +73,7 @@ def upload():
             try:
                 name = row['name']
                 email = row['email']
-                qwiklabs = row['qwiklabURL']
+                qwiklabs = row['qwiklabs']
                 score = getScore(qwiklabs)
                 profile_image = profileImage(qwiklabs)
                 user = models.Leaderboard(name=name, email=email, qwiklab_url=qwiklabs, total_score=score["total_score"], track1_score=score["track1_score"], track2_score=score["track2_score"], profile_image=profile_image)
@@ -120,7 +120,6 @@ def login():
 
 if __name__== "__main__":
     app.run(
-        host='0.0.0.0', port="5000",
+        host='0.0.0.0', port="6363",
+        ssl_context=('origin.pem', 'origin.pem'),
     )
-
-
