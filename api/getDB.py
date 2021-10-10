e1c4d20d1c96b316fcf30e75ca5090b34d0eb642
Raghav Sharma <raghav_tinker@yahoo.com>
2021-10-10 15:41:21 +0530

    not required refresh function in this file

--- api/getDB.py
+++ api/getDB.py
@@ -5,28 +5,6 @@ from app import db
 from getData import getScore, getScoreRefresh, leaderboard, profileImage
 import time
 
-def refreshDb():
-    # get every row from the database
-    data = db.query(models.Leaderboard).all()    
-    # get the data from the database
-    for user in data:
-        print(user.name)
-        # get the score
-        score = getScoreRefresh(user.qwiklab_url)
-        # update the score
-        if score != None:
-            user.track2_score = score['track2_score']
-            user.track1_score = score['track1_score']
-            user.total_score = score['total_score']
-        #user.profile_image = profileImage(user.qwiklab_url)
-        #commit the changes
-            time.sleep(40)
-            db.commit()
-            print("updated")
-        else:
-            print("error")
-            pass
-            
 def getJsonFromDB():
     data = db.query(models.Leaderboard).all()
     data_result = []
