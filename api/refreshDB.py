c54e1d248de3f37f35542ac4a7ffbb9ac8e2e303
Raghav Sharma <raghav_tinker@yahoo.com>
2021-10-10 15:40:30 +0530

    refreshing database script running in bg

---
+++ api/refreshDB.py
@@ -0,0 +1,30 @@
+from app import db
+import models
+from getData import getScoreRefresh
+import time
+
+def refreshDb():
+    # get every row from the database
+    data = db.query(models.Leaderboard).all()    
+    # get the data from the database
+    for user in data:
+        print(user.name)
+        # get the score
+        score = getScoreRefresh(user.qwiklab_url)
+        # update the score
+        if score != None:
+            user.track2_score = score['track2_score']
+            user.track1_score = score['track1_score']
+            user.total_score = score['total_score']
+        #user.profile_image = profileImage(user.qwiklab_url)
+        #commit the changes
+            time.sleep(40)
+            db.commit()
+            print("updated")
+        else:
+            print("error")
+            pass
+    
+while True:
+    refreshDb()
+    time.sleep(60)
